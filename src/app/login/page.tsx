2f90bf17934469a1d452df976fb7dcbcd501142d
Marcos Leva Gomez <marcos@cococo.ventures>
2025-12-26 23:19:27 +0000

    no lo pongas blanco, ponlo azul o de otro color

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,7 @@ const AsteriskIcon = () => (
       width="64"
       height="64"
       viewBox="0 0 64 64"
-      fill="none"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
